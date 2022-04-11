383ec62a936d4b54b4da3cfe63ad13ff799bdd71
ViktorSedlachek <spase.xaoc@gmail.com>
2022-04-11 07:41:58 +0300

    update testsAutomationPracticeForm + RegistrationsFormTests

--- src/test/java/demoqa/RegistrationsFormTests.java
+++ src/test/java/demoqa/RegistrationsFormTests.java
@@ -1,15 +1,14 @@
 package demoqa;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class testsAutomationPracticeForm {
+public class RegistrationsFormTests {
 
     @BeforeAll
     static void setUp() {
@@ -29,30 +28,26 @@ public class testsAutomationPracticeForm {
         open("/automation-practice-form");
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
+
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
         $("#genterWrapper").$(byText("Male")).click();
-        //$("#genter-radio3").parent().click(); - второй способ
         $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("May");
         $(".react-datepicker__year-select").selectOption("2002");
         $(".react-datepicker__day--028:not(react-datepicker__day)--outside-month").click();
-        //$(byText("22")).click(); - дата второй способ
         $("#subjectsInput").setValue(subjects).pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
-        //$("#hobbies-checkbox-1").parent().click(); - второй способ
         $("#uploadPicture").uploadFromClasspath("jpeg/11.jpeg");
-        //$("#uploadPicture").uploadFile(new File("src/test/resources/jpeg/11.jpeg")); второй способ картинки
         $("#currentAddress").setValue(currentAddress);
         $("#state").click();
         $(".stateCity-wrapper").$(byText("NCR")).click();
-        //$(byText("NCR")).click(); - второй способ
         $("#city").click();
         $(".stateCity-wrapper").$(byText("Noida")).click();
-        //$(byText("Noida")).click(); - второй способ
         $("#submit").click();
+
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".modal-body").shouldHave(
                 text("Ivan Ivanov"),
