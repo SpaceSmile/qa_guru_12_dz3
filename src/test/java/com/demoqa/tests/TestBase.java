3735453b7d1b58be263648d255cd4da1ed39ea24
dodoshik <space.xaoc@gmail.com>
2022-05-10 14:17:36 +0300

    config done

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -2,8 +2,10 @@ package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.config.CredentialsConfig;
 import com.demoqa.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,14 +13,22 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
-
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+        String login = config.login();
+        String password = config.password();
+
+        String url = System.getProperty("selenoid", "selenoid.autotests.cloud/wd/hub");
+        String browserSize = System.getProperty("propertyBrowserSize","1980x1024");
+        String baseUrl = System.getProperty("startUrl", "https://demoqa.com");
+
+        Configuration.baseUrl = baseUrl;
+        Configuration.browserSize = browserSize;
+        Configuration.remote = "https://"+ login + ":" + password +"@" + url;
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
